4111a63bc2d86098f904a8e86217fee1bb3e8e9e
김준우 <gimalidjqp@gmail.com>
2026-08-25 21:04:05 +0900

    add dice hold, add Id
    Get에서 POST로 변경, Roll Api 주요 로직 수정

--- app.py
+++ app.py
@@ -11,31 +11,25 @@ users_collection = db['users']
 def home():
     return render_template('Lobby.html')
 
-@app.route('/api/roll', methods=['get'])
+@app.route('/api/roll', methods=['post'])
 def roll_dice():
-    dice_results1 = [random.randint(1, 6) for i in range(5)]
+    dicelist = [random.randint(1, 6)for i in range(5)]
+
     return jsonify({
         'result': 'success',
-        'dice': dice_results1
+        'dicelist': [{'val':dicelist[0], 'hold':False},
+                     {'val':dicelist[1], 'hold':False},
+                     {'val':dicelist[2], 'hold':False},
+                     {'val':dicelist[3], 'hold':False},
+                     {'val':dicelist[4], 'hold':False}]
     })
+
+# async function rollDice() {
+#   const res = await fetch('/api/roll', { method: 'POST' });
+#   const data = await res.json();
+  
+#   console.log(data.dicelist); 
+# }
  
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
-
-    # <script>
-    #     async function rollDice() {
-    #         const response = await fetch('/api/roll', {
-    #             method: 'get'
-    #         });
-    #         const data = await response.json();
-    #         if (data.result === 'success') {
-    #             console.log('주사위 결과:', data.dice);
-    #             // for(let i = 0; i < 5; i++) 
-    #             document.getElementById('dice-result1').innerText = data.dice[0];
-    #             document.getElementById('dice-result2').innerText = data.dice[1];
-    #             document.getElementById('dice-result3').innerText = data.dice[2];
-    #             document.getElementById('dice-result4').innerText = data.dice[3];
-    #             document.getElementById('dice-result5').innerText = data.dice[4];
-    #         }
-    #     }
-    # </script>
