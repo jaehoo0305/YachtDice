46a38f1ff7de99908c9291c8185d842eadc7575a
김준우 <gimalidjqp@gmail.com>
2026-08-26 16:42:25 +0900

    add

--- app.py
+++ app.py
@@ -1,22 +1,27 @@
-import random
+import random, jwt, hashlib, datetime
 from flask import Flask, render_template, jsonify, request
 from pymongo import MongoClient
+from flask_cors import CORS
 
 app = Flask(__name__)
 client = MongoClient('mongodb://localhost:27017/')
 db = client['yacht_dice']
 users_collection = db['users']
+CORS(app)
 
 @app.route('/')
 def home():
     return render_template('Lobby.html')
+
+@app.route('/game')
+def Game():
+    return render_template('Game.html')
 
 @app.route('/api/roll', methods=['post'])
 def roll_dice():
     holdval = request.get_json()
     getdice = holdval.get('dicelist', [])
     new_dicelist = []
-    
     for i in range(5):
         if i < len(getdice) and getdice[i].get('hold') == True:
             new_dicelist.append({
@@ -27,15 +32,17 @@ def roll_dice():
             new_dicelist.append({
                 'val': random.randint(1, 6),
                 'hold': False
-                })
-    return jsonify({'result': 'success', 'dicelist': new_dicelist})
+                }) 
+    combolist =[]
+    if(new_dicelist[i]==1):
+        combolist.append({
+            'yacht':True
+        })
+    else:
+        combolist.append({
+            'yacht':False
+        })
+    return jsonify({'combo':combolist, 'result': 'success', 'dicelist': new_dicelist})
 
-# async function rollDice() {
-#   const res = await fetch('/api/roll', { method: 'POST' });
-#   const data = await res.json();
-  
-#   console.log(data.dicelist); 
-# }
- 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
